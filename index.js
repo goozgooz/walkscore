3391a58ce6f631d02a7c34405d0c2724898c578c
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-08-07 19:17:54 +0000

    working on linear regression stuff

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const getData = require('./lib/import-data.js');
 const parseData = require('./lib/parse-data.js');
 const getWalkscore = require('./lib/walkscore.js');
 const writeData = require('./lib/write-data.js');
+const chart = require('./lib/chart.js');
 
 let walkscoreReport = (csv) => {
   getData(csv)
     .then(parseData)
     .then(getWalkscore)
-    .then(results => {
-      writeData(results,csv);
-    })
+    // .then(results => {
+    //   writeData(results,csv);
+    // })
+    .then(chart)
     .catch(console.log);
 };
 
