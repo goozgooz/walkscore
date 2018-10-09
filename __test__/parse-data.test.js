2bb914421ea29a793db14ee6ec2a68b6b72244e2
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-10-08 20:02:08 -0700

    parse-data.js tested

--- __test__/parse-data.test.js
+++ __test__/parse-data.test.js
@@ -5,9 +5,15 @@ const parseData = require('../lib/parse-data.js');
 const mockCSV = fs.readFileSync(__dirname + '/lib/mock.csv', 'utf8' );
 
 describe('parse-data function', () => {
-  test('should return an object with two keys', () => {
-    let result = parseData(mockCSV);
-    // expect(result).toHaveProperty('headersasdf');
-    expect('derp').toEqual('derp');
+  let result = parseData(mockCSV);
+
+  test('should return an object with the csv headers in one key', () => {
+    expect(result).toHaveProperty('headers');
+    expect(result.headers.length).toEqual(8);
   });
+  test('should return an object with patients in the other key', () => {
+    expect(result).toHaveProperty('patients');
+    expect(result.patients.length).toEqual(2);
+  });
+
 });
